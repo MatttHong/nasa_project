1c6e695416e0f12800a1b9e04bf7c2d7b1cf83a5
MatttHong <matthew.y.hong@vanderbilt.edu>
2022-06-16 14:20:02 +0900

    final- error with clicking abort, numbers dont show

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -33,7 +33,11 @@ return new Promise((resolve, reject) => {
 });
 }
 
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
   module.exports = {
       loadPlanetsData,
-      planets: habitablePlanets,
+      getAllPlanets,
   }
